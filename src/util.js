50e412d95f814e2bb9707b4dcdbc059b78dd55fe
Daniel Gulic <danielgulic@gmail.com>
2018-10-09 16:52:25 +1100

    handle joi function

---
+++ src/util.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+
+exports.handleJoi = (schema, req, res) => {
+  let result = Joi.validate(req.body, schema);
+  if (result.error) {
+      if (!result.error.isJoi) {
+          console.error(`Error while running Joi at ${Date.now()}: ${result.error}`);
+          res.sendStatus(500);
+          return false;
+      }
+      res.status(400).json({ ok: false, errors: result.error.details.map(item => item.message) });
+      return false;
+  } else return true;
+}
