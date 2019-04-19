f4de02b403e4fd797616fac061ee3c44c9e46e11
jellz <danielgulic@gmail.com>
2019-04-19 23:24:49 +1000

    Remove users route from index

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ app.use(express.json());
 app.use(require('cors')());
 
 const port = process.env.PORT || 3000;
-const r = module.exports.r = require('rethinkdbdash')({ db: 'minehutbans' });
+const r = module.exports.r = require('rethinkdbdash')({ db: process.env.DATABASE_NAME });
 
 app.get('/', (req, res) => res.json({ ok: true }));
-app.use('/api/users', require('./routes/users.js'));
 app.use('/api/blacklisted_players', require('./routes/blacklisted_players.js'));
 
 app.listen(port, () => console.log(`minehutbans api listening on ${port}`));
